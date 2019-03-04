55e3c629f06c397430fcb6da87063817a956e33a
way8 <w.a.y@interia.pl>
2019-03-04 22:47:54 +0100

    - filled up morse code - convert plain text to morse code recognised by class Pwm - first attempt to set code thru GUI (but at this moment doesn`t work)

--- src/com/sp9gi/MorseCode.java
+++ src/com/sp9gi/MorseCode.java
@@ -7,12 +7,17 @@ import java.util.Scanner;
  * Class for storage Morse Code
  */
 public class MorseCode {
+
     public MorseCode() {
         executeCode();
-    }
+    } //constructor
 
     String letter;
-    char[] c_arr;
+    char[] in_arr;
+    static char[] co_arr; //at least two object will have access to this variable
+    String code = "";
+    String a = "";
+    String input = "";
 
     // public static void morseData () {
     private static HashMap<String, String> codes = new HashMap<String, String>();
@@ -22,27 +27,66 @@ public class MorseCode {
         codes.put("a", ".-");
         codes.put("b", "-...");
         codes.put("c", "-.-.");
+        codes.put("d", "-..");
+        codes.put("e", ".");
+        codes.put("f", "..-.");
         codes.put("g", "--.");
+        codes.put("h", "....");
         codes.put("i", "..");
-        codes.put("s", "...");
+        codes.put("j", ".---");
+        codes.put("k", "-.-");
+        codes.put("l", ".-..");
+        codes.put("m", "--");
+        codes.put("n", "-.");
+        codes.put("o", "---");
         codes.put("p", ".--.");
+        codes.put("r", ".-.");
+        codes.put("q", "--.-");
+        codes.put("s", "...");
+        codes.put("t", "-");
+        codes.put("u", "..-");
+        codes.put("v", "...-");
+        codes.put("w", ".--");
+        codes.put("x", "-..-");
+        codes.put("y", "-.--");
+        codes.put("z", "--..");
+        codes.put("0", "-----");
+        codes.put("1", ".----");
+        codes.put("2", "..---");
+        codes.put("3", "...--");
+        codes.put("4", "....-");
+        codes.put("5", ".....");
+        codes.put("6", "-....");
+        codes.put("7", "--...");
+        codes.put("8", "---..");
         codes.put("9", "----.");
+        codes.put(".", ".-.-");
+        codes.put(",", "--..--");
+        codes.put("?", "..--..");
+        codes.put(" ", "|");
 
     }
 
-
-
     public void executeCode() {
-        letter = (codes.get("s") + "." + codes.get("p") + "." + codes.get("9") + "." +
-                codes.get("g") + "." + codes.get("i"));
-
-        c_arr = letter.toCharArray();
-
 //        Scanner scan = new Scanner(System.in);
-//        System.out.println("Podaj znak");
+//        System.out.println("wprowadź ciąg znaków");
+//        String input = scan.nextLine();
 
+        in_arr = input.toCharArray();
 
-//        System.out.println("Po morsowskiemu to " + codes.get(letter.toLowerCase()));
+        // Loop for generate morse code with suitable pauses
+        for (char i : in_arr) {
+            if (i == ' ') {
+                a = "";
+            } else a = "_";
+
+            String j = Character.toString(i);
+            code = code + codes.get(j) + a;
+        }
+
+        co_arr = code.toCharArray();
+
+        System.out.println("Po morsowskiemu to " + code);
     }
 
 }
